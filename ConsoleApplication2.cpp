8673abdcd37bab55939651b56b8a1f2bcba3c359
Студент 9/23. РПО <rpo923@step.lan>
2025-01-30 15:07:53 +0700

    second

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -14,10 +14,32 @@ void printBoard(const vector<vector<int>>& board) {
     }
 }
 
+void initializeBoard(vector<vector<int>>& board) {
+    int row, col, value;
+    char continueInput = 'y';
+
+    while (continueInput == 'y') {
+        cout << "Введите строку, столбец и значение (1-9): ";
+        cin >> row >> col >> value;
+
+        if (row >= 1 && row <= SIZE && col >= 1 && col <= SIZE && value >= 1 && value <= 9) {
+            board[row - 1][col - 1] = value;
+        }
+        else {
+            cout << "Некорректный ввод. Попробуйте снова." << endl;
+        }
+
+        cout << "Хотите продолжить ввод? (y/n): ";
+        cin >> continueInput;
+    }
+}
+
 int main() {
     vector<vector<int>> board(SIZE, vector<int>(SIZE, 0));
 
-    cout << "Пустое поле Судоку:" << endl;
+    initializeBoard(board);
+
+    cout << "Поле Судоку с начальными значениями:" << endl;
     printBoard(board);
 
     return 0;
