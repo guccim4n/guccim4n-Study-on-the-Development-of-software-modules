fe33c997817f7c0ecbeb0898471fe3e1d0648426
Студент 9/23. РПО <rpo923@step.lan>
2025-01-30 15:02:35 +0700

    first

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 9;
+
+void printBoard(const vector<vector<int>>& board) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> board(SIZE, vector<int>(SIZE, 0));
+
+    cout << "Пустое поле Судоку:" << endl;
+    printBoard(board);
+
+    return 0;
+}
